0db64d1857fa4aa20e1a43cce1de63686a7aeb21
Maxime Musik <theoloin51@gmail.com>
2026-04-25 21:46:08 +0200

    add netlify domain to CORS

--- server.js
+++ server.js
@@ -16,8 +16,7 @@ const PORT = process.env.PORT || 3000;
 const allowedOrigins = [
   "http://localhost:5500",
   "http://127.0.0.1:5500",
-  "http://localhost:3000",
-  "http://127.0.0.1:3000"
+  "https://caca-simulator.netlify.app"
 ];
 
 app.use(cors({
